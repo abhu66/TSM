0de4ef88cca321527c248366747334e44424a30c
abhu66 <khoirulabhu@gmail.com>
2019-08-26 20:10:27 +0700

    Modified : Tidy Up

--- src/com/project/tsm/base/BaseModel.java
+++ src/com/project/tsm/base/BaseModel.java
@@ -24,57 +24,43 @@ public class BaseModel {
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
-     public String getCode() {
+    public String getCode() {
         return code;
     }
-
     public void setCode(String code) {
         this.code = code;
     }
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public Date getCreatedDate() {
         return createdDate;
     }
-
     public void setCreatedDate(Date createdDate) {
         this.createdDate = createdDate;
     }
-
     public Date getUpdatedDate() {
         return updatedDate;
     }
-
     public void setUpdatedDate(Date updatedDate) {
         this.updatedDate = updatedDate;
     }
-
     public String getCreatedBy() {
         return createdBy;
     }
-
     public void setCreatedBy(String createdBy) {
         this.createdBy = createdBy;
     }
-
     public String getUpdatedBy() {
         return updatedBy;
     }
-
     public void setUpdatedBy(String updatedBy) {
         this.updatedBy = updatedBy;
     }
-    
-    
 }
