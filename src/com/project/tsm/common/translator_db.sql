c351ff2789cc06f82d50386f8f2855edb8b3b90b
=abhu66 <ahmad.dudayef@mitracomm.com>
2019-09-30 10:56:49 +0700

    Modified : update database

--- src/com/project/tsm/common/translator_db.sql
+++ src/com/project/tsm/common/translator_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 11 Sep 2019 pada 12.26
+-- Waktu pembuatan: 30 Sep 2019 pada 05.48
 -- Versi server: 10.1.38-MariaDB
 -- Versi PHP: 5.6.40
 
@@ -48,7 +48,8 @@ INSERT INTO `client` (`id`, `name`, `contact`, `pic`, `address`, `created_date`,
 (1, 'PT.ABCD EFGHI JKLMN', '081218209581', 'ABU KHOERUL ISKANDAR ALI', 'JL. PETA BARAT NO.60 KALIDERES JAKARTA BARAT', '2019-08-28', '2019-08-28', 'admin', 'admin'),
 (2, 'PT. OPQR STUV WXY', '081218209580', 'OSVALDO ERENS', 'DEPOK CIMANGGIS', '2019-08-28', '2019-09-11', 'admin', 'admin'),
 (3, 'PT. ABCD ABCD', '081218209588', 'ADNAN PUTRA', 'CIBUBUR', '2019-08-28', '2019-08-28', 'admin', 'admin'),
-(4, 'PT. AMAN SEJAHTERA', '02154355431', 'ANDRE', 'JAKARTA SELATAN', '2019-09-08', '2019-09-08', 'admin', 'admin');
+(4, 'PT. AMAN SEJAHTERA', '02154355431', 'ANDRE', 'JAKARTA SELATAN', '2019-09-08', '2019-09-08', 'admin', 'admin'),
+(5, 'CHRISTIAN', '081218209581', 'CHRISTIAN', 'MALANG EAST JAVA', '2019-09-16', '2019-09-16', 'admin', 'admin');
 
 -- --------------------------------------------------------
 
@@ -71,8 +72,24 @@ CREATE TABLE `languages` (
 --
 
 INSERT INTO `languages` (`id`, `name`, `description`, `created_date`, `updated_date`, `created_by`, `updated_by`) VALUES
-(1, 'ENG-INA', 'ENGLISH TO INDONESIA', '2019-09-08', '2019-09-08', 'admin', 'admin'),
-(2, 'INA-ENG', 'INDONESIA TO ENGLISH', '2019-09-08', '2019-09-08', 'admin', 'admin');
+(1, 'AHMARIC-ENG', 'AHMARIC TO ENGLISH', '2019-09-30', '2019-09-30', 'admin', 'admin'),
+(2, 'ENG-AHMARIC', 'ENGLISH TO AHMARIC', '2019-09-30', '2019-09-30', 'admin', 'admin'),
+(3, 'AHMARIC-INA', 'AHMARIC TO BAHASA', '2019-09-30', '2019-09-30', 'admin', 'admin'),
+(4, 'INA-AHMARIC', 'BAHASA TO AHMARIC', '2019-09-30', '2019-09-30', 'admin', 'admin'),
+(5, 'ARABIC-ENG', 'ARABIC TO ENGLISH', '2019-09-30', '2019-09-30', 'admin', 'admin'),
+(6, 'ENG-ARABIC', 'ENGLISH TO ARABIC', '2019-09-30', '2019-09-30', 'admin', 'admin'),
+(7, 'ARABIC-INA', 'ARABIC TO BAHASA', '2019-09-30', '2019-09-30', 'admin', 'admin'),
+(8, 'INA-ARABIC', 'BAHASA TO ARABIC', '2019-09-30', '2019-09-30', 'admin', 'admin'),
+(9, 'ABKHAZIAN-ENG', 'ABKHAZIAN TO ENGLISH', '2019-09-30', '2019-09-30', 'admin', 'admin'),
+(10, 'ENG-ABKHAZIAN', 'ENGLISH TO ABKHAZIAN', '2019-09-30', '2019-09-30', 'admin', 'admin'),
+(11, 'ABKHAZIAN-INA', 'ABKHAZIAN TO BAHASA', '2019-09-30', '2019-09-30', 'admin', 'admin'),
+(12, 'INA-ABKHAZIAN', 'BAHASA TO ABKHAZIAN', '2019-09-30', '2019-09-30', 'admin', 'admin'),
+(13, 'ACHOLI-ENG', 'ACHOLI TO ENGLISH', '2019-09-30', '2019-09-30', 'admin', 'admin'),
+(14, 'ENG-ACHOLI', 'ENGLISH TO ACHOLI', '2019-09-30', '2019-09-30', 'admin', 'admin'),
+(15, 'ACHOLI-INA', 'ACHOLI TO BAHASA', '2019-09-30', '2019-09-30', 'admin', 'admin'),
+(16, 'INA-ACHOLI', 'BAHASA TO ACHOLI', '2019-09-30', '2019-09-30', 'admin', 'admin'),
+(17, 'AFAR-ENG', 'AFAR TO ENGLISH', '2019-09-30', '2019-09-30', 'admin', 'admin'),
+(18, 'ENG-AFAR', 'ENGLISH TO AFAR', '2019-09-30', '2019-09-30', 'admin', 'admin');
 
 -- --------------------------------------------------------
 
@@ -86,7 +103,7 @@ CREATE TABLE `order_client` (
   `date_order` date NOT NULL,
   `client_id` int(11) NOT NULL,
   `order_type` varchar(15) DEFAULT NULL,
-  `languange` varchar(30) NOT NULL,
+  `language` int(11) NOT NULL,
   `file_name` varchar(255) DEFAULT NULL,
   `qty` enum('Page','Word') NOT NULL,
   `qty_value` int(11) DEFAULT NULL,
@@ -129,7 +146,9 @@ INSERT INTO `translator` (`id`, `name`, `contact`, `created_date`, `updated_date
 (2, 'AGUNG', '081218209581', '2019-08-31', '2019-08-31', 'admin', 'admin'),
 (3, 'ANA MARIANAS', '08778787134', '2019-08-31', '2019-09-11', 'admin', 'admin'),
 (4, 'RITAS', '089675116160', '2019-08-31', '2019-09-11', 'admin', 'admin'),
-(5, 'ANGGITA WULANDARI', '08121209582', '2019-09-08', '2019-09-08', 'admin', 'admin');
+(5, 'ANGGITA WULANDARI', '08121209582', '2019-09-08', '2019-09-08', 'admin', 'admin'),
+(6, 'NEIDA SHINTIA', '089675116160', '2019-09-11', '2019-09-11', 'admin', 'admin'),
+(7, 'GILANG', '08128292929', '2019-09-16', '2019-09-16', 'admin', 'admin');
 
 -- --------------------------------------------------------
 
@@ -200,13 +219,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `client`
 --
 ALTER TABLE `client`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `languages`
 --
 ALTER TABLE `languages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT untuk tabel `order_client`
@@ -218,7 +237,7 @@ ALTER TABLE `order_client`
 -- AUTO_INCREMENT untuk tabel `translator`
 --
 ALTER TABLE `translator`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
